a01c747b32b652c15158af95574a29a5130b98b6
lukas <lukas@lukas>
2016-01-04 21:10:24 +0000

    class active added

--- assets/includes/marquee.js
+++ assets/includes/marquee.js
@@ -44,6 +44,14 @@ function sliderAdv(){
 
 	if (slider.timePassed == slider.timeToChange ) {
 		slider.timePassed =0;
+		if (slider.autoPlay==true) {
+			if (slider.cSlide==slider.totalSlides) {
+				$('.marquee_nav div:nth-child(1)').trigger('click');
+			}
+			else
+				$('.marquee_nav div:nth-child('+(slider.cSlide+1)+')').trigger('click');
+		};
+
 	} else{
 		slider.timePassed +=1; 
 	}
@@ -66,14 +74,25 @@ function sliderMulti() {
 		slider.timePassed= Math.floor(slider.timeToChange /2);
 	});
 	$('.marquee_nav div').on('click', function(){
-		var navClicked = $(this).index();
-		slider.cSlide = navClicked +1;
 
-		$('.marquee_stage_large').append('<div class="mc2" style="opacity:1"></div>');
-		$('.mc2').html(slider.slideContent[navClicked]).animate({opacity:1},slider.duration,function(){
-			$('.mc1').remove();
-			$(this).addClass('mc1').removeClass('mc2');
-		});
+		if (slider.inTransition==false ) {
+			slider.inTransition=true;
+
+			
+
+			var navClicked = $(this).index();
+			slider.cSlide = navClicked +1;
+
+			$('.marquee_nav div').removeClass('active');
+			$(this).addClass('active');
+
+			$('.marquee_stage_large').append('<div class="mc2" style="opacity:1"></div>');
+			$('.mc2').html(slider.slideContent[navClicked]).animate({opacity:1},slider.duration,function(){
+				$('.mc1').remove();
+				$(this).addClass('mc1').removeClass('mc2');
+				slider.inTransition=false;
+			});
+		}	
 	});
 	$('.marquee_nav div:first').trigger('click');
 }
